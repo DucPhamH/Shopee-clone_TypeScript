80e06f9346402e9ad480257c8d435196d9d314a9
DucPhamH <ducphamhong2@gmail.com>
2023-03-18 21:08:58 +0700

    add headerRegister, footer

--- src/layouts/RegisterLayout/RegisterLayout.tsx
+++ src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -1,11 +1,15 @@
+import Footer from 'src/components/Footer'
+import RegisterHeader from 'src/components/RegisterHeader'
+
 interface Prop {
   children?: React.ReactNode
 }
 export default function RegisterLayout({ children }: Prop) {
   return (
     <div>
-      RegisterLayout
+      <RegisterHeader />
       {children}
+      <Footer />
     </div>
   )
 }
