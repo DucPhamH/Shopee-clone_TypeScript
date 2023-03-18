ff16b8ed0703a3e2acfc22d28af917a00ea31eca
DucPhamH <ducphamhong2@gmail.com>
2023-03-18 20:49:06 +0700

    add router

---
+++ src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -0,0 +1,11 @@
+interface Prop {
+  children?: React.ReactNode
+}
+export default function RegisterLayout({ children }: Prop) {
+  return (
+    <div>
+      RegisterLayout
+      {children}
+    </div>
+  )
+}
