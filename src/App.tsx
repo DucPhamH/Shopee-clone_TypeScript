8f94851e7a13995667cdbe54d323907dffef9ca6
DucPhamH <ducphamhong2@gmail.com>
2023-03-21 17:10:37 +0700

    test api register

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import useRouteElement from './useRouteElement'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const routeElement = useRouteElement()
-  return <div className=''>{routeElement}</div>
+  return (
+    <div>
+      {routeElement}
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App
